12b16b2cdbed81ac862be5b60c7961b9e0648060
admf215 <Jay.Bieshaar@BendigoAdelaide.com.au>
2019-03-07 14:43:15 +1100

    new text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello World
+            Hello World - V2. Cloud Schmoud
           </p>
           <a
             className="App-link"
